e84035b9bb06ba219e91facfa61dc27676d0cf04
DoubleU <yuuliu.5533@gmail.com>
2025-02-17 21:33:33 -0500

    Ground work of landing page, vertical navbar on left, search bar still needs adjusting

--- echonaut/src/app/page.js
+++ echonaut/src/app/page.js
@@ -2,55 +2,106 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-          <li>This is the landing branch</li>
-        </ol>
+    <div className="flex flex-row bg-stone-900">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* Navbar */}
+      <div className="fixed flex flex-col h-screen w-[100px] bg-stone-800">
+
+
+      </div>
+
+      {/* MainPage */}
+      <div className="flex flex-row w-full h-full py-10 justify-center">
+
+        {/* Personal */}
+
+        <div className="flex flex-col h-full w-1/3 gap-3 items-end w-1/3 text-black bg-gray-500">
+          <div className="flex bg-white min-w-[300px] w-[30%] lg:h-[500px]">
+            Hello
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+            Hello
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+            Hello
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+
+        {/* Feed */}
+
+        <div className="flex flex-col h-full w-1/4 gap-3 items-center w-1/3 text-black bg-gray-300">
+          {/*Search */}
+          <div className="relative flex flex-col h-[30px] w-full opacity-[50%] bg-stone-800 rounded-2xl px-8 justify-center">
+            <input type="text" className="bg-transparent border-none focus:outline-none text-white" placeholder="Search . . ."/>
+
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+        </div>
+
+
+        {/* For you */}
+
+        <div className="flex flex-col h-full w-1/3 gap-3 items-start w-1/3 text-black bg-gray-400">
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+        </div>
+
+      </div>
+
+      {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center w-full min-h-screen gap-1  font-[family-name:var(--font-geist-sans)]">
+
+        <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+        </div>
+        
+        <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+          <div className="bg-white min-w-[300px] w-[30%] lg:h-[500px] ">
+
+          </div>
+        </div>
+
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -96,7 +147,10 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+        </footer>
+
+      </div> */}
+
     </div>
   );
 }
