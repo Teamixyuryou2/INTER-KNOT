0ff7ac3a07d64018b16bef9fb98f2b638f69458f
DoubleU <yuuliu.5533@gmail.com>
2025-02-18 22:34:43 -0500

    2/18 Solo development push to main - Flushed out landing page, moved search bar above for you section, recolored navbar, created scrollable feed (Still need to turn into component), made a small mini profile box top left, improved responsive design

--- echonaut/src/app/components/navbar.js
+++ echonaut/src/app/components/navbar.js
@@ -4,34 +4,38 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function Navbar(){
     return(
 
-      <nav className="sticky top-0 left-0 justify-self-start h-screen w-full min-w-[100px] m-0 bg-gray-900 text-white shadow-lg justify-items-center flex flex-col">
+      <nav className="sticky top-0 left-0 justify-self-start h-screen w-full min-w-[100px] m-0 bg-stone-800 text-white shadow-lg justify-items-center flex flex-col">
         <div id="site-logo" className="flex justify-center p-3 text-2xl">
           Echonaut 
         </div>
+        
         <div className="justify-center flex w-full">
-          <div id="home-nav-button" className="flex justify-center xl:justify-start m-1 p-2 h-[40px] w-[90%] hover:brightness-90 hover:cursor-pointer hover:bg-gray-800 rounded-2xl transition-all duration-150 ease-in">
+          <div id="home-nav-button" className="flex justify-center xl:justify-start m-1 p-2 h-[40px] w-[90%] hover:brightness-90 hover:cursor-pointer hover:bg-stone-900 rounded-2xl transition-all duration-150 ease-in">
             <FontAwesomeIcon icon={faHouse} className="scale-[150%] mx-5 mt-[3px]" />
             <div className="hidden xl:block text-xl">
               Home
             </div>
           </div>
         </div>
+        
         <div className="justify-center flex w-full">
-          <div id="msg-nav-button" className="flex justify-center xl:justify-start m-1 p-2 h-[40px] w-[90%] hover:brightness-90 hover:cursor-pointer hover:bg-gray-800 rounded-2xl transition-all duration-150 ease-in">
+          <div id="msg-nav-button" className="flex justify-center xl:justify-start m-1 p-2 h-[40px] w-[90%] hover:brightness-90 hover:cursor-pointer hover:bg-stone-900 rounded-2xl transition-all duration-150 ease-in">
             <FontAwesomeIcon icon={faEnvelope} className="scale-[150%] mx-5 mt-[3px]" />
             <div className="hidden xl:block text-xl">
               Messages
             </div>
           </div>
         </div>
+
         <div className="justify-center flex w-full">
-          <div id="msg-notif-button" className="flex justify-center xl:justify-start m-1 p-2 h-[40px] w-[90%] hover:brightness-90 hover:cursor-pointer hover:bg-gray-800 rounded-2xl transition-all duration-150 ease-in">
+          <div id="msg-notif-button" className="flex justify-center xl:justify-start m-1 p-2 h-[40px] w-[90%] hover:brightness-90 hover:cursor-pointer hover:bg-stone-900 rounded-2xl transition-all duration-150 ease-in">
             <FontAwesomeIcon icon={faBell} className="scale-[150%] mx-5 mt-[3px]" />
             <div className="hidden xl:block text-xl">
               Notifications
             </div>
           </div>
         </div>
+
         <button className="bg-slate-50 text-black w-[90%] p-3 mx-[5%] my-4 block justify-center rounded-xl hover:brightness-[85%] transition-all duration-150 ease-in">
           Echo
         </button>
--- echonaut/src/app/page.js
+++ echonaut/src/app/page.js
@@ -1,67 +1,93 @@
 import Image from "next/image";
 import Navbar from "./components/navbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
+
+
   return (
-    <div className="flex flex-row bg-stone-900 h-screen overflow-hidden">
+    <div className="flex flex-row bg-stone-900 h-screen min-w-[500px] overflow-hidden">
 
       {/* Navbar */}
-      <div className="fixed relative flex flex-col h-full w-[150px] lg:min-w-[100px] lg:w-[17%] bg-stone-800 z-">
+      <div className="fixed relative flex flex-col h-full w-[200px] lg:min-w-[100px] lg:w-[17%] bg-stone-800">
         <Navbar/>
-
       </div>
-      <div className="sticky flex z-50">
-        <div className="fixed flex h-[75px] w-[95%] bg-stone-300 justify-center p-50 opacity-60">
-          <div className="flex flex-col h-[30px] w-[400px] opacity-[50%] bg-stone-800 rounded-2xl px-8 justify-center">
-            <input type="text" className="bg-transparent border-none focus:outline-none text-white" placeholder="Search . . ."/>
-          </div>
-        </div>
-      </div>
+      <div className="flex flex-col w-full">
 
-      {/* MainPage */}
-      <div className="flex flex-row w-full h-full py-10 justify-center">
+        {/* MainPage */}
+        <div className="flex flex-row w-full h-full py-10 justify-center">
 
-        {/* Personal */}
+          {/* Personal */}
 
-        <div className="flex flex-col h-full w-1/3 gap-3 items-end w-1/3 text-black">
-          <div className="hidden xl:block bg-white min-w-[300px] w-[30%] xl:h-[500px]">
-            Hello
-          </div>
-          <div className="hidden xl:block bg-white min-w-[300px] w-[30%] xl:h-[500px] ">
-            Hello
-          </div>
-          <div className="hidden xl:block bg-white min-w-[300px] w-[30%] xl:h-[500px] ">
-            Hello
-          </div>
-        </div>
+          <div className="flex flex-col h-full w-1/3 gap-5 items-end w-1/3 max-w-[600px] text-black md:pr-4">
 
-        {/* Feed */}
+            <div className="hidden lg:block bg-white w-[70%] min-h-[200px] min-w-[250px] lg:h-[500px] rounded-3xl bg-stone-500 overflow-hidden">
 
-        <div className="relative flex flex-col h-full min-w-[400px] w-[60%] lg:w-1/4 gap-3 items-center w-1/3 text-black">
-          {/*Search */}
+              <div className="flex w-full h-1/3 min-h-[50px] bg-white px-4 items-center">
+                {/*Profile Picture */}
+                <div className= "rounded-full bg-stone-200 w-[50px] h-[50px] max-w-[100px] max-h-[100px] min-w-[50px] min-w-[50px]">
 
-          <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-1/2 ">
+                </div>
+                {/*Name*/}
+                <div className= "rounded-full bg-stone-200 w-[200px] h-[40px] min-w-[50px] min-w-[50px] mx-2">
 
-          </div>
-          <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-1/2 ">
+                </div>
+              </div>
+              <div className="h-full w-full bg-stone-300 p-5 text-pretty text-balance">
+                I LOVE AMONGUS I LOVE AMONGUS I LOVE AMONGUS I LOVE AMONGUS I LOVE AMONGUS I LOVE AMONGUS
+              </div>
+
+            </div>
+
+            <div className="hidden lg:block bg-white w-[70%] min-h-[200px] min-w-[250px] lg:h-[500px] rounded-3xl p-6 bg-stone-600">Hello</div>
+            <div className="hidden lg:block bg-white w-[70%] min-h-[200px] min-w-[250px] lg:h-[500px] rounded-3xl p-6 bg-stone-700">Hello</div>
 
           </div>
 
-        </div>
+          {/* Feed */}
 
+          <div className="relative flex flex-col h-screen min-w-[400px] w-[60%] max-w-[400px] lg:w-1/4 gap-3 items-center text-black overflow-y-scroll md:border-r-2 lg:border-x-2 scrollbar-hidden ">
 
-        {/* For you */}
+            <div className="relative flex flex-col min-w-[400px] w-[60%] lg:w-1/4 my-5 items-center text-black space-y-4 md:border-r-2 lg:border-x-2">
+              {/* Turn this into a component */}
+              <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-[500px] bg-stone-400">a</div>
+              <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-[500px] bg-stone-500">b</div>
+              <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-[500px] bg-stone-600">c</div>
+              <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-[500px] bg-stone-700"></div>
+              <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-[500px] bg-stone-400"></div>
+              <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-[500px] bg-stone-500"></div>
+              <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-[500px] bg-stone-600"></div>
+              <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-[500px] bg-stone-700"></div>
+              <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-[500px] bg-stone-400"></div>
+              <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-[500px] bg-stone-500"></div>
+              <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-[500px] bg-stone-600"></div>
+              <div className="bg-white min-w-[300px] w-[90%] max-w-[600px] h-[500px] bg-stone-700"></div>
+            </div>
 
-        <div className="flex flex-col h-full w-1/3 gap-3 items-start w-[50%] lg:w-1/3 text-black ">
-          <div className="hidden md:block bg-white min-w-[200px] w-[70%]  h-[500px]">
-            Hey
           </div>
-          <div className="hidden md:block bg-white min-w-[200px] w-[70%] h-[500px] ">
+
+          
+
+
+          {/* For you */}
+          
+          <div className="flex flex-col h-full w-1/3 gap-5 items-start w-[50%] max-w-[600px] lg:w-1/3 text-black pl-4">
+
+            {/*Search*/}
+            <div className="flex h-[100px] w-[70%] min-w-[200px] items-center top-0 left-0 opacity-60 hidden md:block">
+              <div className="flex flex-row h-[40px] w-full opacity-[50%] focus:opacity-[100%] bg-stone-200 rounded-3xl justify-center items-center px-2">
+                <FontAwesomeIcon icon={faMagnifyingGlass} className="w-[40px] h-[40px] " />
+                <input type="text" className="bg-transparent h-[30px] w-full border-none focus:outline-none text-white invert" placeholder="Search . . ."/>
+              </div>
+            </div>
+
+            <div className="hidden md:block bg-white min-w-[200px] w-[70%] min-h-[200px] min-w-[200px] h-[500px] rounded-3xl p-6 bg-stone-500">Hey</div>
+            <div className="hidden md:block bg-white min-w-[200px] w-[70%] min-h-[200px] min-w-[200px] h-[500px] rounded-3xl p-6 bg-stone-600"></div>
+            <div className="hidden md:block bg-white min-w-[200px] w-[70%] min-h-[200px] min-w-[200px] h-[500px] rounded-3xl p-6 bg-stone-700"></div>
 
           </div>
-          <div className="hidden md:block bg-white min-w-[200px] w-[70%]  h-[500px] ">
 
-          </div>
         </div>
 
       </div>
