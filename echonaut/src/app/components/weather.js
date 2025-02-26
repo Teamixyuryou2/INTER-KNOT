362e0c767148815581c1f0a132f6f116cf3e1386
Carl Feng <carlfeng3@gmail.com>
2025-02-26 00:00:47 -0500

    Working weather widget with clock
    API calls now work, and clock in weather widget updates

--- echonaut/src/app/components/weather.js
+++ echonaut/src/app/components/weather.js
@@ -1,47 +1,68 @@
 "use client"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Weather(){
     const apiKey = "62c8370009022c37b85d7235fdb4dfac";
     let city = "New York"
 
-    async function getWeatherData(city){
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const [currWeather, setWeather] = useState({});
+
+    async function getWeatherData(){
         try{
-            const response = await fetch(apiUrl);
-            const weatherData = await response.json();
-            console.log(weatherData);
-            return weatherData;
-        }   
-        catch(error){
-            throw new error("Could not fetch weather data")
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setWeather(data)
+            });
+        } catch {
+            
         }
+    };
+
+    useEffect(() => {
+        getWeatherData();
+    }, []);
+   
+    // Clock
+    const [time, setTime] = useState(new Date());
+    function formatTime(){
+        let hours = time.getHours().toString().padStart(2, "0");
+        let minutes = time.getMinutes().toString().padStart(2, "0");
+        let seconds = time.getSeconds().toString().padStart(2, "0");
+        let day = time.getDay();
+        let month = time.getMonth();
+        let date = time.getDate();
+        let dayString = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        let monthString = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        
+        return `${hours}:${minutes}:${seconds} - ${dayString[day]} ${monthString[month]} ${date}`;
+    }
+    
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalID);
+        }
+    }, []);
+
+    if(!currWeather || !currWeather.main){
+        return <div className="flex justify-center items-center text-center text-white text-xl">Loading Weather Data...</div>
     }
 
-    // getWeatherData.then(
-
-    // )
-    useEffect(()=>{
-        getWeatherData(city);
-    },[])
-
-    console.log(getWeatherData("New York"));
-    //  function displayWeatherInfo(){
-    //     const weatherData = getWeatherData("New York");
-    //     const weatherData2 = {name: city,
-    //         main: {temp, humidity},
-    //     }
-    //  }
+    const tempRounded = currWeather.main ? Math.round(currWeather.main.temp) : null;
 
     return(
         <div id="widget-wrapper" className="flex flex-col w-full h-full bg-transparent text-white text-center items-center justify-center font-[Rubik] font-normal select-none">
             <div id="weather-graphic" className="scale-[200%] p-4">☀️</div>
-            <div id="temperature" className="flex text-5xl xl:text-[70px] justify-center p-0 xl:p-1">
-                69°F    
+            <div id="temperature" className="flex text-4xl xl:text-5xl 2xl:text-[70px] justify-center p-0 xl:p-1">
+                {tempRounded != null ? `${tempRounded}°F` : "Loading..."}
             </div>
-            <div id="city-name" className="text-xl xl:text-3xl p-1">{getWeatherData.coord}</div>
-            <div id="date-time" className="text-lg xl:text-xl p-2">06:09 - Monday June 9</div>
-            <div className="text-lg xl:text-xl">Ether particle concentration: 11%</div>
+            <div id="city-name" className="text-xl xl:text-2xl 2xl:text-3xl p-1">New Eridu</div>
+            <div id="date-time" className="text-base xl:text-xl p-2">{formatTime()} </div>
+            <div className="text-base xl:text-xl">Ether particle concentration: {currWeather.main.humidity}%</div>
         </div>
     )
 }
